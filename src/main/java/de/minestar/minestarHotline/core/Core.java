5a6ba1fc69db2d8802fcb95774a23facc5d644db
Meldanor <Bhaals@gmx.de>
2012-05-18 00:03:33 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/minestarHotline/core/Core.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of MinestarHotline.
+ * 
+ * MinestarHotline is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MinestarHotline is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MinestarHotline.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.minestarHotline.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class Core extends AbstractCore {
+
+    public static final String NAME = "MinestarHotline";
+
+    public Core() {
+        super(NAME);
+    }
+
+}
