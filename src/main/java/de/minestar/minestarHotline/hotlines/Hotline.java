35dce5b3ceef9a296289d580f9cd08ac5a3829c2
Meldanor <Bhaals@gmx.de>
2012-05-18 01:59:04 +0200

    Add interface for the Hotlines
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/minestarHotline/hotlines/Hotline.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of MinestarHotline.
+ * 
+ * MinestarHotline is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MinestarHotline is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MinestarHotline.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.minestarHotline.hotlines;
+
+public interface Hotline {
+
+    public void sendMessage(String callerName, String message);
+
+    public void registerSupporter(String supportName, String contactInformation);
+    
+    public void unregisterSupporter(String suppportName, String contactInformation);
+
+}
